7b4625884bb6264a246983015fcc081415db6203
detanx <1286335864@qq.com>
2020-04-26 10:40:58 +0800

    Update webpack.analyz.js

--- config/webpack.analyz.js
+++ config/webpack.analyz.js
@@ -1,5 +1,21 @@
 // webpack.analyz.js
+const path = require('path');
 const merge = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // 分析我们打包的代码
 const prod = require('./webpack.prod');
+// 从根目录开始
+function resolve(dir) {
+  return path.join(__dirname, '..', dir);
+}
 
-module.exports = merge(prod, {});
+module.exports = merge(prod, {
+  output: {
+    path: resolve('analyz')
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled', // 避免每次启动都打开分析网站
+      generateStatsFile: true
+    })
+  ]
+});
